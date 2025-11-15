b937515bdde52c511be1698910e75651d08825de
Dyrhoi <39205283+Dyrhoi@users.noreply.github.com>
2025-11-15 14:55:49 +0100

    use happy-dom and optimize antd

--- packages/antd-crux/vitest.config.ts
+++ packages/antd-crux/vitest.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        environment: "jsdom",
-        globals: true,
-        typecheck: {
-            enabled: true,
-        },
+  plugins: [react()],
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    typecheck: {
+      enabled: true,
     },
+    deps: {
+      optimizer: {
+        client: {
+          enabled: true,
+          include: ["antd"],
+        },
+      },
+    },
+  },
 });
